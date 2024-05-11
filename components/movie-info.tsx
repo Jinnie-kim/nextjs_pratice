3e2a6a9b089a1ffa6fe4e2565e229257355d3e73
Jinnie <hjk7639@gmail.com>
2024-05-11 14:48:12 +0900

    051124 - CSS: Movie Trailers

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,12 +1,25 @@
 import { API_URL } from '../app/(home)/page';
+import style from '../styles/movie-info.module.css';
 
 async function getMovie(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
 }
 
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
-  return <h1>{JSON.stringify(movie)}</h1>;
+  return (
+    <div className={style.container}>
+      <img src={movie.poster_path} className={style.poster} alt={movie.title} />
+      <div>
+        <h1 className={style.title}>{movie.title}</h1>
+        <h3>⭐️{movie.vote_average.toFixed(1)}</h3>
+        <p className={style.info}>{movie.overview}</p>
+        <a href={movie.homepage} target={'_blank'}>
+          Homepage →
+        </a>
+      </div>
+    </div>
+  );
 }
